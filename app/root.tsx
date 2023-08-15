7215e1ab6b4ac2616eb474fa4cc8119f799f0510
David <j.david.prado1716@gmail.com>
2023-08-15 11:32:36 -0500

    Handling Error Responses

--- app/root.tsx
+++ app/root.tsx
@@ -2,12 +2,14 @@ import { cssBundleHref } from "@remix-run/css-bundle";
 import styles from './styles/main.css'
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
 import MainNavigation from "./components/MainNavigation";
 
@@ -15,6 +17,63 @@ export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [{ rel: "stylesheet", href: styles }]),
 ];
 
+export function ErrorBoundary({error}) {
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Meta />
+      <Links />
+      <title> An error ocurred! </title>
+    </head>
+    <body>
+      <header>
+        <MainNavigation />
+      </header>
+      <main className = 'error'>
+        <h1> An Error Ocurred! </h1>
+        <p> {error.message}</p>
+        <p> Back to <Link to='/' > safety</Link>! </p>
+      </main>
+      <Outlet />
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
+  )
+}
+
+export function CatchBoundary () {
+  const caughtResponse = useCatch()
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Meta />
+      <Links />
+      <title> {caughtResponse.statusText} </title>
+    </head>
+    <body>
+      <header>
+        <MainNavigation />
+      </header>
+      <main className = 'error'>
+        <h1> An Error Ocurred! </h1>
+        <p> {caughtResponse.statusText}</p>
+        <p> {caughtResponse.data?.message || 'Something went wrong!'}</p>
+        <p> Back to <Link to='/' > safety</Link>! </p>
+      </main>
+      <Outlet />
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
+  )
+}
 export default function App() {
   return (
     <html lang="en">
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,11 +1,20 @@
-import { redirect } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
 import NewNote, {links as NewNoteLinks} from '../components/NewNote'
 import {getStoredNotes, storeNotes} from "../data/note.js"
 import NoteList, {links as noteListLinks} from '~/components/NoteList';
-import { useLoaderData } from '@remix-run/react';
+import { Link, useCatch, useLoaderData } from '@remix-run/react';
 
 export async function loader() {
   const notes = await getStoredNotes();
+  if (!note || notes.lenght === 0) {
+    throw json(
+      {message: "Could not find any note."},
+      {
+        status: 404,
+        statusText: 'Not Foundito' 
+      },
+    )
+  }
   return notes;
 }
 
@@ -30,6 +39,29 @@ export async function action({request}) {
 export function links() {
     return [...NewNoteLinks(), ...noteListLinks()];
   }
+
+export function ErrorBoundary({error}) {
+  return (
+    <main className = 'error'>
+      <h1> An Error releted to your notes Ocurred! </h1>
+      <p> {error.message}</p>
+      <p> Back to <Link to='/' > safety</Link>! </p>
+    </main>
+  )
+}
+
+export function CatchBoundary () {
+  const caughtResponse = useCatch ()
+
+  const message = caughtResponse.data?.message || 'data not found'
+
+  return (
+    <main>
+      <NewNote />
+      <p className='info-message'>{message}</p>
+    </main>
+  )
+}
 
 export default function notes() {
 
